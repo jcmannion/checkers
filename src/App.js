9fe290b8877bd18908c1e85c271133e9a31fc3a5
john mannion <jcmannion21@gmail.com>
2022-02-16 19:08:38 -0500

    fix errors in src/App.js

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  for (let i = 1; i <= 8; i++){
+//   for (let i = 1; i <= 8; i++){
 
-    //=======Append 8 vertical rows to the body=======//
+//     //=======Append 8 vertical rows to the body=======//
     
-        $('#game').append('<div class="row-'+i+'" style="display: inline-block; margin-top: -4px"></div>');
-        for (let j = 1; j <= 8; j++){
-            if (i%2!=0){
-                if (j%2!=0){
-                    color='black';
-                }else{
-                    color='red';
-                }
-            } else {
-                if (j%2==0){
-                    color='black';
-                }else{
-                    color='red';
-                }
-            }
+//         $('#game').append('<div class="row-'+i+'" style="display: inline-block; margin-top: -4px"></div>');
+//         for (let j = 1; j <= 8; j++){
+//             if (i%2!=0){
+//                 if (j%2!=0){
+//                     color='black';
+//                 }else{
+//                     color='red';
+//                 }
+//             } else {
+//                 if (j%2==0){
+//                     color='black';
+//                 }else{
+//                     color='red';
+//                 }
+//             }
     
-    //=======Append 8 boxes to the 8 rows, alternating them black and red=======//
+//     //=======Append 8 boxes to the 8 rows, alternating them black and red=======//
     
-            $('.row-'+i).append('<div class="boxes box-'+box+'" style="background-color: '+color+'; width: 50px; height: 50px; padding-top: 3px"></div>');
-            box++;
-        }
-    }
+//             $('.row-'+i).append('<div class="boxes box-'+box+'" style="background-color: '+color+'; width: 50px; height: 50px; padding-top: 3px"></div>');
+//             box++;
+//         }
+//     }
   return (
     <div className="App">
       <header className="App-header">
